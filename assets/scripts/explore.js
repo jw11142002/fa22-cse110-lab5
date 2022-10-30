d31e87cd96e4ee3bfb00a8c71398dfd3bdc765fb
jw11142002 <jw11142002@gmail.com>
2022-10-29 23:18:55 -0700

    add expose.js

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,4 +4,38 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  const synth = window.speechSynthesis;
+
+  const voiceSelect = document.querySelector('select');
+  //const rate = document.querySelector('#rate');
+  //const rateValue = document.querySelector('.rate-value');
+
+  let voices = [];
+
+  function populateVoiceList() {
+    voices = synth.getVoices();
+
+    for (let i = 0; i < voices.length ; i++) {
+      const option = document.createElement('option');
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+      if (voices[i].default) {
+        option.textContent += ' — DEFAULT';
+      }
+
+      option.setAttribute('data-lang', voices[i].lang);
+      option.setAttribute('data-name', voices[i].name);
+      voiceSelect.appendChild(option);
+    }
+  }
+
+  populateVoiceList();
+
+  const pressTalk = document.querySelector('button');
+  let text = document.getElementById("text-to-speak").value;
+
+  pressTalk.addEventListener('click', (event) => {
+    let utterance = new SpeechSynthesisUtterance(text);
+    window.speechSynthesis.speak(utterance);
+  })
 }
